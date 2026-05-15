5546b2b950f0d0ca95b43e84cbe74545a1bbb4b5
Karthikeyan <karthikeyanagaram2004@gmail.com>
2026-05-15 23:19:59 +0530

    fixed neon db connection

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://vizhuthugal-backend.onrender.com/api',
+  baseURL: 'https://vizhuthugal-backend-3jmj.onrender.com/api',
 });
 
 export default API;
